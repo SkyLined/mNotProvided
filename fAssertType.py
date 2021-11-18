c19f24add9c0896ef21ad34e390339351dbe60c4
SkyLined <github@skylined.nl>
2021-11-18 15:24:55 +0100

    __mDebugOutput_HideInCallStack -> mDebugOutput_HideInCallStack

--- fAssertType.py
+++ fAssertType.py
@@ -7,7 +7,7 @@ def fAssertType(sName, xValue, *txTypes):
   # variable is of an incorrect type. Ideally, we would do this inline in the caller function and throw the exception
   # from there, but Python does not allow us to easily do that. So we hide this function in the stack in mDebugOutput:
   # This causes the exception throw from this function if the variable type is invalid to appear as if it was thrown
-  __mDebugOutput_HideInCallStack = True; # from the function calling this function.
+  mDebugOutput_HideInCallStack = True; # from the function calling this function.
   def fbIsOfType(xValue, *txTypes):
     for xType in txTypes:
       if xType is None:
--- fAssertTypes.py
+++ fAssertTypes.py
@@ -5,7 +5,7 @@ def fAssertTypes(dtxValueAndValueTypes_by_sName):
   # variable is of an incorrect type. Ideally, we would do this inline in the caller function and throw the exception
   # from there, but Python does not allow us to easily do that. So we hide this function in the stack in mDebugOutput:
   # This causes the exception throw from this function if the variable type is invalid to appear as if it was thrown
-  __mDebugOutput_HideInCallStack = True; # from the function calling this function.
+  mDebugOutput_HideInCallStack = True; # from the function calling this function.
   # Argument is {"name": (value, type, ....), ...}
   for (sName, txValueAndValueTypes) in dtxValueAndValueTypes_by_sName.items():
     # Call fAssertType for each entry in the dict.
